57af636fd11a49e055227ef7245f582ec0c8a451
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 01:59:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] = "Assembly".unshift
-     programmer_hash
+     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 
 end
